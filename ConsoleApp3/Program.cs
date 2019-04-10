994ddc0ce99e54a1a8ecfcf60a599e8d1ebef192
visualstudio <visualstudio@122-PC12>
2019-04-10 11:54:44 -0400

    sec 4

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,15 +1,91 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp3
+namespace ConsoleApp2
 {
     class Program
     {
+        ArrayList Beowulf;
+
         static void Main(string[] args)
         {
+            Program p = new Program();
+
+
+            p.Beowulf = new ArrayList();
+
+
+            p.ReadTextFiles();
+
+
+        }
+
+        public void Run()
+        {
+            this.ReadTextFiles();
+        }
+
+        public void ReadTextFiles()
+        {
+
+            using (StreamReader file = new StreamReader(@"U:\Users\731555\beowulf.txt"))
+            {
+                int counter = 0;
+
+
+
+                int number = 0;
+
+
+                string line;
+
+
+                while ((line = file.ReadLine()) != null)
+
+                {
+                    if (line.Contains("Fare") == true && line.Contains("War") == false || line.Contains("Fare") == true && line.Contains("War") == false)
+
+                    {
+                        int x = counter - 1;
+
+
+                        number++;
+
+
+                    }
+                    counter++;
+
+                }
+                Console.WriteLine($"The number of lines that contains *Fare* without the word *War* are {number}");
+
+
+
+                file.Close();
+            }
+        }
+        public int FindNumberOfBlankSpace(string line)
+        {
+
+            int countletters = 0;
+
+
+            int countSpaces = 0;
+
+            foreach (Char c in line)
+            {
+                if (char.IsLetter(c)) { countletters++; }
+
+
+                if (char.IsWhiteSpace(c)) { countSpaces++; }
+
+
+            }
+            return countSpaces;
         }
     }
 }
